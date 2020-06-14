20f2cbf15ad840a2afade9cc769323658495c170
Dm6 <munyoudoum@gmail.com>
2020-06-14 19:39:45 +0700

    added comments

--- motion.py
+++ motion.py
@@ -1,22 +1,25 @@
+# 02/2020
+# code from https://youtu.be/MkcUgPhOlP8
+# I modify the code to fit my needs
+# send a message as picture on Facebook when there is any motion from the camera
 import cv2
 import numpy as np
+# not official from facebook
 from fbchat import Client
 from fbchat.models import *
-import fbchat
+import fbchat       
 from getpass import getpass
 from datetime import datetime
 
-cap = cv2.VideoCapture(0)  # 0 or ip address
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap = cv2.VideoCapture(0)  # 0 to open laptop cam or ip address
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
-user = "munyoudoum"
+user = "munyoudoum" # your facebook username
+# type your password
 client = fbchat.Client(user, getpass())
-
 
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
@@ -29,13 +32,17 @@ while cap.isOpened():
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
 
-        if cv2.contourArea(contour) < 900:
+        if cv2.contourArea(contour) < 900: # 900 = size of motion
             continue
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), \
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        # check when the time(seconds) is  between 00s 10s
+        
         if "00" < datetime.now().strftime("%S") < "10":
+            # create a picture file
             cv2.imwrite('01.png', frame1)
+            # send a message on Facebook
             client.sendLocalImage(
                 "01.png",
                 message=Message(text="MOTION DETECTED"),
@@ -50,7 +57,7 @@ while cap.isOpened():
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
-
+    # press esc to stop the camera
     if cv2.waitKey(40) == 27:
         break
 client.logout()
